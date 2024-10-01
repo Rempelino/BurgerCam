be43c2d6049e44ee08542952a2b2dec85a936b84
Rempelino <henry.rempel1996@gmail.com>
2024-10-01 10:08:33 +0200

    modified enviroment

--- frontend/src/enviroments/eniroment.prod.ts
+++ frontend/src/enviroments/eniroment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://192.168.100.55:57000'
+    //apiUrl: 'http://192.168.100.55:57000'
+    apiUrl: 'http://localhost:57000'
   };
--- frontend/src/enviroments/enviroment.ts
+++ frontend/src/enviroments/enviroment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-   // apiUrl: 'http://localhost:57000'
-    apiUrl: 'http://192.168.100.55:57000'
+    apiUrl: 'http://localhost:57000'
   };
