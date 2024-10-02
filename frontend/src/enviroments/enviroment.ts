aef75d6ecdc69f0d3c4577f90c5226c140911b15
Rempelino <henry.rempel1996@gmail.com>
2024-10-02 11:58:06 +0200

    added proxy config

--- frontend/src/enviroments/enviroment.ts
+++ frontend/src/enviroments/enviroment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://192.168.100.55:57000'
+    //apiUrl: 'http://192.168.100.55:57000'
+    //apiUrl: 'http://localhost:57000'
+    apiUrl: '/api'
   };
